64d5cc60aa61eda6fc28cd9892d265724f5b08d3
Rakshith Bellare <rb@nuiteq.com>
2020-08-22 22:28:30 +0530

    Some css styling

--- script.js
+++ script.js
@@ -15,21 +15,29 @@ var app = new Vue({
       }
     },
     convertRGBtoHEX() {
+      if(this.colorValue.startsWith('#')) {
+        this.convertedValue = "Enter a rgb color value separated by commas";
+        return;
+      }
       this.colorValue = this.colorValue.split(",");
       this.colorValue = this.colorValue.map((item) => Number(item));
       this.convertedValue = this.rgbToHex(this.colorValue[0], this.colorValue[1], this.colorValue[2]);
-      let root = document.documentElement;
+      const root = document.documentElement;
       root.style.setProperty('--maindiv-bgcolor', this.convertedValue);
     },
     convertHEXtoRGB() {
+      if(!this.colorValue.startsWith('#')) {
+        this.convertedValue = "Enter a hex value starting with a #";
+        return;
+      }
       var result = /^#?([a-f\d]{2})([a-f\d]{2})([a-f\d]{2})$/i.exec(this.colorValue);
       this.convertedValue = result ? { 
         r: parseInt(result[1], 16),
         g: parseInt(result[2], 16),
         b: parseInt(result[3], 16)
       } : null;
-      let rgbString = `rgb(${this.convertedValue.r},${this.convertedValue.g},${this.convertedValue.b})`;
-      let root = document.documentElement;
+      const rgbString = `rgb(${this.convertedValue.r},${this.convertedValue.g},${this.convertedValue.b})`;
+      const root = document.documentElement;
       root.style.setProperty('--maindiv-bgcolor', rgbString);
     },
     rgbToHex(r, g, b) {
@@ -41,10 +49,15 @@ var app = new Vue({
       return hex.length == 1 ? "0" + hex : hex;
     }
   },
-  mounted() {
-    fetch('https://source.unsplash.com/random')
-      .then(function(response) {
-          document.getElementById('app').style.backgroundImage = `url(${response.url})`;
-      });
+//  mounted() {
+//    fetch('https://source.unsplash.com/random')
+//      .then(function(response) {
+//          document.getElementById('app').style.backgroundImage = `url(${response.url})`;
+//      });
+//  },
+  watch: {
+    convertRGB() {
+      this.colorValue = '';
+    }
   }
 });
